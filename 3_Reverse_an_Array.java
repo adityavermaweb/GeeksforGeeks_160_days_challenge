05d82f203d3206ebed74fc283fff8d0ecbe71916
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-03-31 12:12:50 +0000

    container config done

---
+++ 3_Reverse_an_Array.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static void reverseArray(int arr[]) {
+        int left = 0, right = arr.length -1;
+        while(left < right) {
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+            
+        }
+    }
+    public static void main(String[] arge){
+        int[] arr = {1, 4, 3, 2, 6, 5};
+        reverseArray(arr);
+        System.out.println(Arrays.toString(arr));
+    }
