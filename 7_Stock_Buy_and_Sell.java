239eaa04414c74c213cf0b21162b279915b7163b
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-04-06 05:21:35 +0000

    container config done

--- 7_Stock_Buy_and_Sell.java
+++ 7_Stock_Buy_and_Sell.java
@@ -5,3 +5,12 @@ class Solution {
         
         int minPrice = prices[0];
         int maxProfit = 0;
+
+        for (int i = 1; i < prices.length; i++) {
+            int profit = prices[i] - minPrice;
+            maxProfit = Math.max(maxProfit, profit);
+            minPrice = Math.min(minPrice, prices[i]);
+        }
+        return maxProfit;
+    }
+}
