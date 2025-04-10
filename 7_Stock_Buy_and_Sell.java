8e3385ba58c29fcdc24a1edc4ea66e7d8c5f3deb
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-04-10 16:14:02 +0000

    container config done

--- 7_Stock_Buy_and_Sell.java
+++ 7_Stock_Buy_and_Sell.java
@@ -9,8 +9,7 @@ class Solution {
             if (prices[i] > prices[i - 1]) {
                 maxProfit += prices[i] - prices[i - 1];
             }
-        }
-        
+        }        
         return maxProfit;
-    }
+        }
 }
