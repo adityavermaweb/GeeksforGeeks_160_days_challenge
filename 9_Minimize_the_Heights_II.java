e634cd8289680f2b74201fac4b9a3242f3ef1877
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-04-14 16:40:18 +0000

    container config done

--- 9_Minimize_the_Heights_II.java
+++ 9_Minimize_the_Heights_II.java
@@ -9,3 +9,15 @@ class Solution {
         
         int smallest = arr[0] + k;
         int largest = arr[n - 1] - k;
+        for (int i = 0; i < n - 1; i++) {
+            int min = Math.min(smallest, arr[i + 1] - k);
+            int max = Math.max(largest, arr[i] + k);
+            
+            if (min < 0 ) continue;
+            
+            ans = Math.min(ans, max - min);
+        }
+        
+        return ans;
+    }
+}
