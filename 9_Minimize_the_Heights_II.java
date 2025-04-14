ec7db4f8158bb27c57efe3bc0022092837582c25
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-04-14 16:38:44 +0000

    container config done

--- 9_Minimize_the_Heights_II.java
+++ 9_Minimize_the_Heights_II.java
@@ -1,1 +1,11 @@
 // --------------------------MINIMIZE THE HEIGHTS II ------------------------------->
+class Solution {
+    int getMinDiff(int[] arr, int k) {
+        int n = arr.length;
+        if (n == 1) return 0;
+        
+        Arrays.sort(arr);
+        int ans = arr[n - 1] - arr[0];
+        
+        int smallest = arr[0] + k;
+        int largest = arr[n - 1] - k;
