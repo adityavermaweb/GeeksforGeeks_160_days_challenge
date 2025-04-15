332781a5c5adf2b386171fcd6afa663e46edc689
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-04-15 17:56:09 +0000

    container config done

--- 10_Kadane's_Algorithm.java
+++ 10_Kadane's_Algorithm.java
@@ -1,1 +1,14 @@
 // --------------------------Kadane's Algorithm----------------------->
+class Solution {
+    int maxSubarraySum(int[] arr) {
+        int maxSoFar = arr[0];
+        int currentMax = arr[0];
+        
+        for (int i = 1; i < arr.length; i++) {
+            currentMax = Math.max(arr[i], currentMax + arr[i]);
+            maxSoFar = Math.max(maxSoFar, currentMax);
+        }
+        
+        return maxSoFar;
+    }
+}
