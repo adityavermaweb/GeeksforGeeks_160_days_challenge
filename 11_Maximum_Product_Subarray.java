cc06666292785f93042f6f05b3c3bff3a6706453
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-04-16 17:23:42 +0000

    container config done

--- 11_Maximum_Product_Subarray.java
+++ 11_Maximum_Product_Subarray.java
@@ -1,1 +1,10 @@
 // --------------------------Maximum Product Subarray------------------------>
+class Solution {
+    // Function to find maximum product subarray
+    int maxProduct(int[] arr) {
+        // code here
+        int n = arr.length;
+        int maxProduct = arr[0];
+        int maxEndingHere = arr[0];
+        int minEndingHere = arr[0];
+        
