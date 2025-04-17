41af4929059d43105a93d134667e32c852daa54a
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-04-17 18:21:37 +0000

    container config done

--- 12_Max_Circular_Subarray_Sum.java
+++ 12_Max_Circular_Subarray_Sum.java
@@ -1,1 +1,20 @@
 // ----------------------------- MAX CIRCUALR SUBARRAY SUM ----------------------->
+class Solution {
+
+    // a: input array
+    // n: size of array
+    // Function to find maximum circular subarray sum.
+    public int circularSubarraySum(int arr[]) {
+
+        // Your code here
+        int totalSum = 0;
+        int maxSum = kadane(arr);
+        int minSum = kadaneMin(arr);
+        
+        for (int num : arr) {
+            totalSum += num;
+        }
+        if(maxSum < 0) return maxSum;
+        
+        return Math.max(maxSum, totalSum - minSum);
+    }
