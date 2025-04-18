f8ba0c3581fb8f7cc8e7cfdbe8e5f5a352f062f1
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-04-18 18:11:34 +0000

    container config done

---
+++ 13_Smallest_Positive_Missing.java
@@ -0,0 +1,7 @@
+// ---------------------------Smallest Positive Missing ------------------------------->
+
+class Solution {
+    // Function to find the smallest positive number missing from the array.
+    public int missingNumber(int[] arr) {
+        // Your code here
+        int n = arr.length;
