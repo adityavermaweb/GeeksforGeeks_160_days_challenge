52cba6d0c2f3d1526e393baf7ecb2f4c459702a4
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-04-19 18:12:56 +0000

    container config done

--- 14_Implement_Atoi.java
+++ 14_Implement_Atoi.java
@@ -6,7 +6,7 @@ class Solution {
         int n = s.length();
         int sign = 1;
         long result = 0;
-        
+
         while (i < n && s.charAt(i) == ' ') {
             i++;
         }
@@ -15,3 +15,20 @@ class Solution {
             sign = s.charAt(i) == '-' ? -1 : 1;
             i++;
         }
+        while (i < n && s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+            int digit = s.charAt(i) - '0';
+            result = result * 10 + digit;
+            
+            if (sign == 1 && result > Integer.MAX_VALUE) {
+                return Interger.MAX_VALUE;
+            }
+            if(sign == -1 && -result < Integer.MIN_VALUE) {
+                return Integer.MIN_VALUE;
+            }
+            
+            i++;
+        }
+        
+        return (int) (sign * result);
+    }
+}
