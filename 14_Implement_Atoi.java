8720b7aed07317b88899655679270eb2edba8c0a
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-04-19 18:11:31 +0000

    container config done

---
+++ 14_Implement_Atoi.java
@@ -0,0 +1,8 @@
+// -------------------------------------------IMPLEMENT ATOI ----------------------------------->
+class Solution {
+    public int myAtoi(String s) {
+        // Your code here
+        int i = 0;
+        int n = s.length();
+        int sign = 1;
+        long result = 0;
