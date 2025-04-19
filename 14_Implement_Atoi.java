7c66724893eb47f9e905ed6b5a44d2c3cc2531ab
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-04-19 18:12:08 +0000

    container config done

--- 14_Implement_Atoi.java
+++ 14_Implement_Atoi.java
@@ -6,3 +6,12 @@ class Solution {
         int n = s.length();
         int sign = 1;
         long result = 0;
+        
+        while (i < n && s.charAt(i) == ' ') {
+            i++;
+        }
+        
+        if (i < n && (s.charAt(i) == '+' || s.charAt(i) == '-')) {
+            sign = s.charAt(i) == '-' ? -1 : 1;
+            i++;
+        }
