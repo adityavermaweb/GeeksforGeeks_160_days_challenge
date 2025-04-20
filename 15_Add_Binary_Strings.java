c4db89045f090e6fc05d537f5288c8f5dfa4e840
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-04-20 17:57:56 +0000

    container config done

--- 15_Add_Binary_Strings.java
+++ 15_Add_Binary_Strings.java
@@ -1,1 +1,8 @@
 // --------------------ADD BINARY STRINGS------------------------------->
+class Solution {
+    public String addBinary(String s1, String s2) {
+        // code here
+        int i = s1.length() - 1;
+        int j = s2.length() - 1;
+        int carry = 0;
+        StringBuilder result = new StringBuilder();
