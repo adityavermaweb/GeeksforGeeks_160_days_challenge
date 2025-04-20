c4dde17ae8f44245470e4fba2f4fca5b9c35ef2e
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-04-20 17:59:36 +0000

    container config done

--- 15_Add_Binary_Strings.java
+++ 15_Add_Binary_Strings.java
@@ -10,9 +10,9 @@ class Solution {
 
         while (i >= 0 || j >= 0 || carry != 0) {
            int bit1 = i >= 0 ? s1.charAt(i) - '0' : 0;
-            int bit2 = j >= 0 ? s2.charAt(j) - '0' : 0;
+           int bit2 = j >= 0 ? s2.charAt(j) - '0' : 0;
             
-            int sum = bit1 + bit2 + carry;
+           int sum = bit1 + bit2 + carry;
             result.append(sum % 2);
             carry = sum / 2;
             
