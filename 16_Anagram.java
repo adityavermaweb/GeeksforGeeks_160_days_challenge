7f333fedb8f076dde391a19345e7ca0eea840665
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-04-21 17:25:11 +0000

    container config done

--- 16_Anagram.java
+++ 16_Anagram.java
@@ -7,3 +7,8 @@ class Solution {
         if (s1.length() != s2.length()) {
             return false;
         }
+        char[] arr1 = s1.toCharArray();
+        char[] arr2 = s2.toCharArray();
+        
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
