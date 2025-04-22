dd86aa7ca9a4f708c4990a17908ce1dedee28273
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-04-22 17:57:29 +0000

    container config done

--- 17_Non_Repeating_Character.java
+++ 17_Non_Repeating_Character.java
@@ -6,3 +6,11 @@ class Solution {
         for (int i = 0; i < s.length(); i++) {
             freq[s.charAt(i) - 'a']++;
         }
+        for (int i = 0; i < s.length(); i++) {
+            if(freq[s.charAt(i) - 'a'] == 1) {
+                return s.charAt(i);
+            }
+        }
+        return'$';
+    }
+}
