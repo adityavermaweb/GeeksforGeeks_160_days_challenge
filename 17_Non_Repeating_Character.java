b6ad068b8b03b13b8b79dbbb29a8c9799a3abf3b
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-04-22 17:56:54 +0000

    container config done

--- 17_Non_Repeating_Character.java
+++ 17_Non_Repeating_Character.java
@@ -1,1 +1,8 @@
 // -----------------------------NON REPEATING CHARACTER ----------------------------------------->
+class Solution {
+    static char nonRepeatingChar(String s) {
+        // code here
+        int [] freq = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            freq[s.charAt(i) - 'a']++;
+        }
