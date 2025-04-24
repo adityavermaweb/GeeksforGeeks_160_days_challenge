8b763cc966b97e0082ae8d66138aab64492660d6
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-04-24 19:08:14 +0000

    container config done

--- 18_Search_Pattern(KMP_Algorithm).java
+++ 18_Search_Pattern(KMP_Algorithm).java
@@ -1,6 +1,25 @@
 // ---------------------------------SEARCH PATTERN (KMP ALGORITHM)--------------------------->
- private int[] computeLPS(String pat) {
+class Solution {
+    
+    private int[] computeLPS(String pat) {
         int m = pat.length();
         int[] lps = new int[m];
         int len = 0;
         int i = 1;
+        
+        while (i < m) {
+            if (pat.charAt(i) == pat.charAt(len)) {
+                len++;
+                lps[i] = len;
+                i++;
+            } else {
+                if (len != 0) {
+                    len = lps[len - 1];
+                } else {
+                    lps[i] = 0;
+                    i++;
+                }
+            }
+        }
+        return lps;
+    }
