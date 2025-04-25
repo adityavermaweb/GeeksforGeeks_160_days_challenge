75e6167b88bdee4957b659bdf302f6f3fe73bc59
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-04-25 18:21:14 +0000

    container config done

--- 19_Min_Chars_to_Add_for_Palindrom.java
+++ 19_Min_Chars_to_Add_for_Palindrom.java
@@ -1,1 +1,11 @@
 // ------------------------MIN CHARS TO ADD FOR PALINDROM -------------------------------->
+class Solution {
+    public static int minChar(String s) {
+        // Write your code here
+        StringBuilder rev = new StringBuilder(s).reverse();
+        String combined = s + "$" + rev;
+        
+        int[] lps = computeLPS(combined);
+        
+        return s.length() - lps[combined.length() - 1];
+    }
