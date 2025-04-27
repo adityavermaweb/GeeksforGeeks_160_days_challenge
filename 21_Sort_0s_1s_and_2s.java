a13c83cfe1a11cd5cce5f74202f515cc1986b83c
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-04-27 17:59:49 +0000

    container config done

--- 21_Sort_0s_1s_and_2s.java
+++ 21_Sort_0s_1s_and_2s.java
@@ -1,1 +1,14 @@
 // -----------------------------SORT 0s, 1s AND 2s---------------------------->
+class Solution {
+    // Function to sort an array of 0s, 1s, and 2s
+    public void sort012(int[] arr) {
+        // code here
+        int low = 0, mid = 0, high = arr.length - 1;
+        
+        while (mid <= high) {
+            if (arr[mid] == 0) {
+                int temp = arr[low];
+                arr[low] = arr[mid];
+                arr[mid] = temp;
+                low++;
+                mid++;
