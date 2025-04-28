3799264f15fdd360dc19ac42a9f8d9d0c8c03cd3
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-04-28 19:34:03 +0000

    container config done

--- Find_H-Index.java
+++ Find_H-Index.java
@@ -4,3 +4,16 @@ class Solution {
     public int hIndex(int[] citations) {
         // code here
         Arrays.sort(citations);
+        int n = citations.length;
+        int h = 0;
+        
+        for (int i = 0; i < n; i++) {
+            int papers = n - i;
+            if (citations[i] >= papers) {
+                h = papers;
+                break;
+            }
+        }
+        return h;
+    }
+}
