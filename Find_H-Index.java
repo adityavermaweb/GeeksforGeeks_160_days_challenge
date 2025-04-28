d0a69ed91b672d808e50c504601d357d15706e8e
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-04-28 19:33:29 +0000

    container config done

---
+++ Find_H-Index.java
@@ -0,0 +1,6 @@
+// --------------------------Find H-Index------------------------>
+class Solution {
+    // Function to find hIndex
+    public int hIndex(int[] citations) {
+        // code here
+        Arrays.sort(citations);
