fb32d4046bc46705dade107fa622dfe6dfbc9485
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-05-02 18:54:00 +0000

    container config done

--- 24_Overlapping_Intervals.java
+++ 24_Overlapping_Intervals.java
@@ -1,5 +1,10 @@
+// -----------------------------Overlapping Intervals-------------------------->
+
 class Solution {
     public List<int[]> mergeOverlap(int[][] arr) {
         // Code here // Code here
         List<int[]> result = new ArrayList<>();
         if (arr == null || arr.length == 0) return result;
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
+        int[] current = arr[0];
+        result.add(current);
