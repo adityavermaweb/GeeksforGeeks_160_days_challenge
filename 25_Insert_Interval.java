bba2df39c09fe0231dbd81947a8ea4baa7b79272
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-05-03 14:10:40 +0000

    container config done

--- 25_Insert_Interval.java
+++ 25_Insert_Interval.java
@@ -10,3 +10,9 @@ class Solution {
             result.add(intervals[i]);
             i++;
         }
+
+        while (i < n && intervals[i][0] <= newInterval[1]) {
+            newInterval[0] = Math.min(newInterval[0], intervals[i][0]);
+            newInterval[1] = Math.max(newInterval[1], intervals[i][0]);
+            i++;
+        }
