a537be6ca446d3ddd1227539f9ceb60c785b44d9
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-05-03 14:09:28 +0000

    container config done

--- 25_Insert_Interval.java
+++ 25_Insert_Interval.java
@@ -1,1 +1,7 @@
 // --------------------------Insert Interval --------------------------
+class Solution {
+    static ArrayList<int[]> insertInterval(int[][] intervals, int[] newInterval) {
+        // code here
+        ArrayList<int[]> result = new ArrayList<>();
+        int i = 0;
+        int n = intervals.length;
