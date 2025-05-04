56bd1126e0abae0220f2531433a1c3318685f4cb
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-05-04 16:49:44 +0000

    container config done

--- 26_Non-overlapping_Intervals.java
+++ 26_Non-overlapping_Intervals.java
@@ -5,3 +5,6 @@ class Solution {
         if (intervals.length == 0) return 0;
         
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
+
+        int count = 0;
+        int prevEnd = intervals[0][1];
