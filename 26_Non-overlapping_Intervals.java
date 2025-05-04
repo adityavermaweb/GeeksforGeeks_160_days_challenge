7538e676147052bb75b58723c99a3be90bdafa18
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-05-04 16:50:12 +0000

    container config done

--- 26_Non-overlapping_Intervals.java
+++ 26_Non-overlapping_Intervals.java
@@ -8,3 +8,12 @@ class Solution {
 
         int count = 0;
         int prevEnd = intervals[0][1];
+        for(int i = 1; i < intervals.length; i++) {
+            if (intervals[i][0] < prevEnd) {
+                count++;
+            } else {
+                prevEnd = intervals[i][1];
+            } 
+        }
+        
+        return count;
