c6aa27407056c691fd03f4580f140587986d3956
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-05-04 16:49:15 +0000

    container config done

--- 26_Non-overlapping_Intervals.java
+++ 26_Non-overlapping_Intervals.java
@@ -1,1 +1,7 @@
 // ---------------------Non-overlapping Intervals----------------------->
+class Solution {
+    static int minRemoval(int intervals[][]) {
+        // code here
+        if (intervals.length == 0) return 0;
+        
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[1], b[1]));
