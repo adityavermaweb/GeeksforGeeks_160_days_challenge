b22872ab632fb56ad0b014af4ba21b0c3131214e
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-05-06 19:20:25 +0000

    container config done

--- 27_Merge_Without_Extre_Space.java
+++ 27_Merge_Without_Extre_Space.java
@@ -9,3 +9,16 @@ class Solution {
         
         while (gap > 0) {
             int i = 0, j = gap;
+
+             while (j < n + m) {
+                int val1 = getValue(a, b, i, n);
+                int val2 = getValue(a, b, j, n);
+                
+                if (val1 > val2) {
+                    setValue(a, b, i, val2, n);
+                    setValue(a, b, j, val1, n);
+                    
+                }
+                i++;
+                j++;
+            }
