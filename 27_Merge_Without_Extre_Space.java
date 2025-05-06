1078b49269465bf19f485b0351524c1ca3ec4d9a
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-05-06 19:19:49 +0000

    container config done

--- 27_Merge_Without_Extre_Space.java
+++ 27_Merge_Without_Extre_Space.java
@@ -1,1 +1,11 @@
 // ----------------------Merge Without Extra Space--------------------->
+class Solution {
+    // Function to merge the arrays.
+    public void mergeArrays(int a[], int b[]) {
+        // code here
+        int n = a.length;
+        int m = b.length;
+        int gap = nextGap(n + m);
+        
+        while (gap > 0) {
+            int i = 0, j = gap;
