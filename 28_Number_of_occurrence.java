7fcc0dd3d1007784b54c89ba6c3d27915a48d5f2
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-05-07 18:40:24 +0000

    container config done

--- 28_Number_of_occurrence.java
+++ 28_Number_of_occurrence.java
@@ -7,3 +7,18 @@ class Solution {
         int last = findLast(arr, target);
         return last - first + 1;
     }
+    private int findFirst(int[] arr, int target) {
+        int low = 0, high = arr.length - 1, result = -1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (arr[mid] == target) {
+                result = mid;
+                high = mid - 1;
+            } else if (arr[mid] < target) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
+        return result;
+    }
