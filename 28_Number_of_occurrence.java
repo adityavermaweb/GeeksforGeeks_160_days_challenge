8106dce47dcc35edfc667fea20fcfb4b4dc8a0cd
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-05-07 18:39:24 +0000

    container config done

--- 28_Number_of_occurrence.java
+++ 28_Number_of_occurrence.java
@@ -1,1 +1,9 @@
 // ----------------------Number of Occurrence--------------->
+class Solution {
+    int countFreq(int[] arr, int target) {
+        // code here
+        int first = findFirst(arr, target);
+        if (first == -1) return 0;
+        int last = findLast(arr, target);
+        return last - first + 1;
+    }
