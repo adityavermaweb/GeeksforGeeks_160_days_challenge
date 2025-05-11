79282fe03ab717335ee33e2786b4567e2cd53f90
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-05-11 08:06:11 +0000

    container config done

--- 29_Sorted_and_Rotated_Minimum.java
+++ 29_Sorted_and_Rotated_Minimum.java
@@ -5,3 +5,9 @@ class Solution {
         int low = 0, high = arr.length - 1;
         while (low < high) {
             int mid = low + (high - low) / 2;
+
+            if (arr[mid] > arr[high]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
