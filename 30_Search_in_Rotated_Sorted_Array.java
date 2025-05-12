4250a6ec9efd7f724dc5c4076ade4937f12645e3
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-05-12 18:13:05 +0000

    container config done

--- 30_Search_in_Rotated_Sorted_Array.java
+++ 30_Search_in_Rotated_Sorted_Array.java
@@ -1,1 +1,6 @@
 // ----------------------Search in Rotated Sorted Array------------------>
+class Solution {
+    int search(int[] arr, int key) {
+        // Complete this function
+        int n = arr.length;
+        int low = 0, high = n - 1;
