b439a0473d241059c074e0e9121fcfb5184020e5
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-05-17 19:03:02 +0000

    container config done

--- 31_Peak_element.java
+++ 31_Peak_element.java
@@ -7,3 +7,18 @@ class Solution {
         
         while (low <= high) {
             int mid = low + (high - low) / 2;
+
+            boolean left = (mid == 0) || (arr[mid] > arr[mid - 1]);
+            boolean right = (mid == arr.length - 1) || (arr[mid] > arr[mid + 1]);
+            
+            if (left && right) {
+                return mid;
+            } else if (mid > 0 && arr[mid - 1] > arr[mid]) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return -1;
+    }
+}
