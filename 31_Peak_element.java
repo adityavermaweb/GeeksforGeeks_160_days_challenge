8cdc54b76719a689269c9d05b56ebfd8883db315
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-05-17 19:02:02 +0000

    container config done

--- 31_Peak_element.java
+++ 31_Peak_element.java
@@ -1,1 +1,9 @@
 // --------------------------Peak Element------------------->
+class Solution {
+
+    public int peakElement(int[] arr) {
+        // code here
+        int low = 0, high = arr.length - 1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
