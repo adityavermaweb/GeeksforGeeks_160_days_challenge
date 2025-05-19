dcb1de7d8bbd36e2c309872c952ce905c2f6e1ee
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-05-19 19:27:15 +0000

    container config done

--- 32_K-th_element_of_two_Arrays.java
+++ 32_K-th_element_of_two_Arrays.java
@@ -1,1 +1,11 @@
 // ---------------K-th element of two Arrays-------------------->
+
+class Solution {
+    public int kthElement(int a[], int b[], int k) {
+        // code here
+        int n = a.length;
+        int m = b.length;
+        
+        if (n > m) {
+            return kthElement(b, a, k);
+        }
