5f581f688e686b5927e03661a7314fb91791bfe2
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-05-19 19:28:22 +0000

    container config done

--- 32_K-th_element_of_two_Arrays.java
+++ 32_K-th_element_of_two_Arrays.java
@@ -15,3 +15,8 @@ class Solution {
         while (low <= high) {
             int cut1 = (low + high) / 2;
             int cut2 = k - cut1;
+             
+            int l1 = (cut1 == 0) ? Integer.MIN_VALUE : a[cut1 - 1];
+            int l2 = (cut2 == 0) ? Integer.MIN_VALUE : b[cut2 - 1];
+            int r1 = (cut1 == n) ? Integer.MAX_VALUE : a[cut1];
+            int r2 = (cut2 == m) ? Integer.MAX_VALUE : b[cut2];
