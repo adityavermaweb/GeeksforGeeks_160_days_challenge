775129704c0c4713d6c7a513127f90c3fc1fa582
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-05-20 17:56:12 +0000

    container config done

--- 33_Aggressive_Cows.java
+++ 33_Aggressive_Cows.java
@@ -1,1 +1,9 @@
 // -----------------Aggressive Cows---------------->
+class Solution {
+    public static int aggressiveCows(int[] stalls, int k) {
+        // code here
+        Arrays.sort(stalls);
+        
+        int low = 1;
+        int high = stalls[stalls.length - 1] - stalls[0];
+        int ans = 0;
