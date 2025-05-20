a6fc365b4133f5968d9ccf6a6decf58f47ffc483
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-05-20 17:57:41 +0000

    container config done

--- 33_Aggressive_Cows.java
+++ 33_Aggressive_Cows.java
@@ -7,3 +7,15 @@ class Solution {
         int low = 1;
         int high = stalls[stalls.length - 1] - stalls[0];
         int ans = 0;
+
+        while (low <= high) {
+           int mid = low + (high - low) / 2;
+           if (canPlaceCows(stalls, k, mid)) {
+               ans = mid;
+               low = mid + 1;
+            } else {
+               high = mid - 1;
+            }
+        }
+        return ans;
+    }
