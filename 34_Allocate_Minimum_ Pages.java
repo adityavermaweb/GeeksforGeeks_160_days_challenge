7e44dc19d60e3e59f9424c8b94d01bb6ac1a1cf9
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-05-21 18:05:43 +0000

    container config done

--- 34_Allocate_Minimum_ Pages.java
+++ 34_Allocate_Minimum_ Pages.java
@@ -1,1 +1,11 @@
 // ------------------Allocate Minimum Pages-------------->
+class Solution {
+    public static int findPages(int[] arr, int k) {
+        // code here
+        if (arr.length < k) return -1;
+        
+        int low = 0, high = 0;
+        for(int pages : arr) {
+            low = Math.max(low, pages);
+            high += pages;
+        }
