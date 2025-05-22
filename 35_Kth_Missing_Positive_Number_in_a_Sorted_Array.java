e9e73e6ee9799105a8bba5199bbce3314948ad9f
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-05-22 03:49:50 +0000

    container config done

--- 35_Kth_Missing_Positive_Number_in_a_Sorted_Array.java
+++ 35_Kth_Missing_Positive_Number_in_a_Sorted_Array.java
@@ -1,1 +1,8 @@
 // ------------------Kth Missing Positive Number in a Sorted Array------------------->
+class Solution {
+    public int kthMissing(int[] arr, int k) {
+        // code here
+        int n = arr.length;
+        int missingCount = 0;
+        int current = 1;
+        int i = 0;
