d1240c647c7663b5070aeb4f7499a691a54536bb
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-05-22 03:51:29 +0000

    container config done

--- 35_Kth_Missing_Positive_Number_in_a_Sorted_Array.java
+++ 35_Kth_Missing_Positive_Number_in_a_Sorted_Array.java
@@ -6,3 +6,11 @@ class Solution {
         int missingCount = 0;
         int current = 1;
         int i = 0;
+        while (missingCount < k) {
+            if (i < n && arr[i] == current) {
+               i++;
+            } else {
+                missingCount++;
+                if (missingCount == k) {
+                   return current;
+                } 
