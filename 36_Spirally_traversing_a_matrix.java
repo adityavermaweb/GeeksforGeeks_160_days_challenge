3063db75a70f384992aefc1a2bc856a6b9baaa15
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-06-03 15:57:53 +0000

    container config done

--- 36_Spirally_traversing_a_matrix.java
+++ 36_Spirally_traversing_a_matrix.java
@@ -6,3 +6,14 @@ class Solution {
         
         if (mat == null || mat.length == 0)
         return result;
+
+        int top = 0;
+        int bottom = mat.length - 1;
+        int left = 0;
+        int right = mat[0].length - 1;
+        
+        while (top <= bottom && left <= right) {
+            for (int i = left; i <= right; i++) {
+                result.add(mat[top][i]);
+            }
+            top++;
