1be5965b78550aeef98f43398a78280f2573b3e9
Aditya Verma <138436209+adityavermaweb@users.noreply.github.com>
2025-08-05 18:08:22 +0000

    container config done

--- 41_Container_With_Most_Water.java
+++ 41_Container_With_Most_Water.java
@@ -3,3 +3,15 @@ class Solution {
         int maxArea = 0;
         int left = 0;
         int right = height.length-1;
+        while (left < right) {
+            int currentArea = (right - left) * Math.min(height[left],height[right]);
+            maxArea = Math.max(maxArea, currentArea);
+            if(height[left] <= height[right]){
+                left ++;
+            }else{
+                right--;
+            }
+        }
+        return maxArea;
+    }
+}
